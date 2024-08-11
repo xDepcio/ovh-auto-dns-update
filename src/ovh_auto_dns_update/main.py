970871d811dfff9c8f2f07c9de3191fcdddbbba3
xDepcio <olek.drwal@gmail.com>
2024-08-12 01:42:02 +0200

    updated main

--- src/ovh_auto_dns_update/main.py
+++ src/ovh_auto_dns_update/main.py
@@ -54,8 +54,7 @@ class AppDirHandler:
 class OVHApiController:
 
     @staticmethod
-    def delete_A_record(domain: str, subdomain: str):
-        record_id = OVHApiController.get_A_record_id(domain, subdomain)
+    def delete_A_record(domain: str, record_id: str):
         return client.delete(f"/domain/zone/{domain}/record/{record_id}")
 
     @staticmethod
@@ -63,6 +62,12 @@ class OVHApiController:
         result = client.get(
             f"/domain/zone/{domain}/record", fieldType="A", subDomain=subdomain
         )
+        if type(result) != list:
+            raise Exception("Error while fetching A record ID")
+
+        if len(result) == 0:
+            return None
+
         return result[0]
 
     @staticmethod
@@ -75,8 +80,8 @@ class OVHApiController:
         )
 
     @staticmethod
-    def refresh_dns_zone():
-        return client.post("/domain/zone/adrwal.pl/refresh")
+    def refresh_dns_zone(domain: str):
+        return client.post(f"/domain/zone/{domain}/refresh")
 
 
 @app.command()
@@ -84,6 +89,15 @@ def update(
     domain: Annotated[str, typer.Option()],
     subdomains_list: Annotated[
         List[str], typer.Option("--subdomain", "-s", help="Subdomains to update")
+    ],
+    application_key: Annotated[
+        str, typer.Option("--application-key", "-k", help="Application key")
+    ],
+    application_secret: Annotated[
+        str, typer.Option("--application-secret", "-s", help="Application secret")
+    ],
+    consumer_key: Annotated[
+        str, typer.Option("--consumer-key", "-c", help="Consumer key")
     ],
     force: bool = False,
 ):
@@ -97,10 +111,11 @@ def update(
 
     if prev_ip != current_ip or force:
         for subdomain in subdomains_list:
-            OVHApiController.delete_A_record(domain, subdomain)
+            record_id = OVHApiController.get_A_record_id(domain, subdomain)
+            if record_id:
+                OVHApiController.delete_A_record(domain, record_id)
             OVHApiController.add_A_record(domain, subdomain, current_ip)
-
-        OVHApiController.refresh_dns_zone()
+            OVHApiController.refresh_dns_zone(domain)
 
         with open(AppDirHandler.get_prev_ip_file_path(), "w") as f:
             f.write(current_ip)
@@ -109,6 +124,15 @@ def update(
         return
 
     typer.echo("No changes detected.")
+
+
+@app.command()
+def test():
+    record = OVHApiController.get_A_record_id("adrwal.pl", "strapup")
+    print(record)
+    # print(OVHApiController.delete_A_record("adrwal.pl", None))
+    # print(OVHApiController.add_A_record("adrwal.pl", "xd2", "8.8.8.8"))
+    OVHApiController.refresh_dns_zone("adrwal.pl")
 
 
 if __name__ == "__main__":
