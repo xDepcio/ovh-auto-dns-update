7c34cdc7b457b588abe332836cf76405c571f0b8
xDepcio <olek.drwal@gmail.com>
2024-08-12 00:55:51 +0200

    cleanued up main

--- src/ovh_auto_dns_update/main.py
+++ src/ovh_auto_dns_update/main.py
@@ -2,8 +2,6 @@
 First, install the latest release of Python wrapper: $ pip install ovh
 """
 
-from dataclasses import dataclass
-import json
 from typing import Annotated, List
 import ovh
 from dotenv import load_dotenv
@@ -13,49 +11,13 @@ import requests
 
 load_dotenv()
 app = typer.Typer()
-# Instantiate an OVH Client.
-# You can generate new credentials with full access to your account on
-# the token creation page (https://api.ovh.com/createToken/index.cgi?GET=/*&PUT=/*&POST=/*&DELETE=/*)
+
 client = ovh.Client(
     endpoint="ovh-eu",  # Endpoint of API OVH (List of available endpoints: https://github.com/ovh/python-ovh#2-configure-your-application)
     application_key=os.getenv("OVH_APPLICATION_KEY"),
     application_secret=os.getenv("OVH_APPLICATION_SECRET"),
     consumer_key=os.getenv("OVH_CONSUMER_KEY"),
 )
-
-result = client.get("/domain")
-
-# Pretty print
-print(json.dumps(result, indent=4))
-
-result2 = client.get("/domain/zone/adrwal.pl/record/")
-print(json.dumps(result2, indent=4))
-
-result3 = client.get("/domain/zone/adrwal.pl/record/5316053875/")
-print(json.dumps(result3, indent=4))
-
-# postResult = client.post("/domain/zone/adrwal.pl/record/", target="8.8.8.8", subDomain="olek-test", fieldType='A')
-
-# print(json.dumps(postResult, indent=4))
-
-# refreshResult = client.post("/domain/zone/adrwal.pl/refresh")
-
-# # Pretty print
-# print(json.dumps(refreshResult, indent=4))
-
-resultRecords = client.get(
-    "/domain/zone/adrwal.pl/record", fieldType="A", subDomain="strapup"
-)
-
-
-@dataclass
-class Config:
-    domain: str
-    subdomains: list[str]
-
-
-# Pretty print
-print(json.dumps(resultRecords, indent=4))
 
 
 def get_current_ip():
